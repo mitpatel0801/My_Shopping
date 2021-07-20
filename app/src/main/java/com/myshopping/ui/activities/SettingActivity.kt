1aae944d008a65c7c39c9fed203a3440e892f589
Mit <mit0801@my.yorku.ca>
2021-07-20 19:48:36 +0530

    Logout Feature Created

--- app/src/main/java/com/myshopping/ui/activities/SettingActivity.kt
+++ app/src/main/java/com/myshopping/ui/activities/SettingActivity.kt
@@ -1,26 +1,36 @@
 package com.myshopping.ui.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.myshopping.R
 import com.myshopping.firestore.FirestoreClass
 import com.myshopping.models.User
 import com.myshopping.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_setting.*
 
-class SettingActivity : BaseActivity() {
+class SettingActivity : BaseActivity(), View.OnClickListener {
+
+    private lateinit var mUser: User
+    private val TAG = "AAA"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
         setupActionBar()
+        Log.d(TAG, "onCreate: ")
 
+        btn_logout.setOnClickListener(this)
+        tv_edit.setOnClickListener(this)
     }
 
 
     override fun onResume() {
         super.onResume()
         getUserData()
+        Log.d(TAG, "onResume: ")
 
     }
 
@@ -31,6 +41,7 @@ class SettingActivity : BaseActivity() {
 
     fun userLoggedInSuccess(user: User) {
         hideProgressDialog()
+        mUser = user
         GlideLoader(this).loadUserPicture(user.image, iv_user_photo)
         tv_name.text = "${user.firstName} ${user.lastName}"
         tv_email.text = user.email
@@ -53,5 +64,40 @@ class SettingActivity : BaseActivity() {
         toolbar_settings_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_logout -> {
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
+            R.id.tv_edit -> {
+                TODO("Make Intent for starting User Profile and make changes UserProfileActivity accordingly ")
+            }
+        }
+    }
 
 }
